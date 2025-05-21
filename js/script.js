94e19950dd471402589c60d65ef15381953af4d7
curtiselfn <curtis.elfn@gmail.com>
2025-05-21 22:58:48 +0800

    created the car object

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ const person = {
 
 console.log(person.name)
 console.log(person["age"])
+
+const dog = {
+    name: "Buddy",
+    breed: "Labrador",
+    bark: function () {
+        console.log("Woof!");
+    }
+}
+
+dog.bark();
+
+const car = {
+    brand: "Toyota",
+    year: 2020,
+    color: "black"
+}
+
+console.log(car.color);
+console.log(car["color"])
